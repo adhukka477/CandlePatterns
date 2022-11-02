a94ec8f48697f7cafbe778f8af06d0f3422fb64d
adhukka477 <alishandhukka@gmail.com>
2022-11-01 22:29:54 -0500

    Complete function for marubuzo pattern

--- Patterns.py
+++ Patterns.py
@@ -1,5 +1,6 @@
 from metrics import *
 from stock_history import Ticker
+import numpy as np
 
 class Patterns(Ticker):
 
@@ -11,18 +12,27 @@ class Patterns(Ticker):
     def flagType(self):
         self.data["Type"] = [1 if self.data.loc[i, "Close"] >= self.data.loc[i, "Open"] else 0 for i in range(len(self.data))]
     
-    def longFlag(self, x = 20, alpha = 2):
+    def longFlag(self, n = 20, alpha = 2):
         #   Calculate ATR(x)
-        atr = self.calculator.calculateATR(self.data, x)
+        atr = self.calculator.calculateATR(self.data, n)
         self.data["LongFlag"] = [1 if abs(self.data.loc[i, "Close"]-self.data.loc[i, "Open"]) >= alpha*atr[i] else 0 for i in range(len(self.data))]
 
-    def shortFlag(self, x = 20, alpha = 0.5):
+    def shortFlag(self, n = 20, alpha = 0.5):
         #   Calculate ATR(x)
-        atr = self.calculator.calculateATR(self.data, x)
+        atr = self.calculator.calculateATR(self.data, n)
         self.data["ShortFlag"] = [1 if abs(self.data.loc[i, "Close"]-self.data.loc[i, "Open"]) <= alpha*atr[i] else 0 for i in range(len(self.data))]
 
-    def marubozuFlag(self):
-        pass
+    def marubozuFlag(self, alpha = 2, n = 20):
+        atr = self.calculator.calculateATR(self.data, n)
+        long = [True if abs(self.data.loc[i, "Close"]-self.data.loc[i, "Open"]) >= alpha*atr[i] else False for i in range(len(self.data))]
+        max_body = [self.data.loc[i, "Open"] if self.data.loc[i, "Open"] >= self.data.loc[i, "Close"] else self.data.loc[i, "Close"] for i in range(len(self.data))]
+        min_body = [self.data.loc[i, "Open"] if self.data.loc[i, "Open"] <= self.data.loc[i, "Close"] else self.data.loc[i, "Close"] for i in range(len(self.data))]        
+        upper_shadow = [max_body[i]/self.data.loc[i, "High"] for i in range(len(self.data))]
+        lower_shadow = [min_body[i]/self.data.loc[i, "Low"]  for i in range(len(self.data))]
+        no_upper_shadow = [True if x >= 0.95 else False for x in upper_shadow]
+        no_lower_shadow = [True if x <= 1.05 else False for x in lower_shadow]
+        self.data["MarubozuFlag"] = [1 if no_upper_shadow[i] and no_lower_shadow[i] and long[i] else 0 for i in range(len(self.data))]
+
 
     def closingMarubozuFlag(self):
         pass
